8e7995c539e6c3f73ea80c44d167d2423c6df75f
guille pacheco <byguille6@gmail.com>
2021-03-25 13:11:18 +0100

    v1

---
+++ cliente/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.Conectar = new System.Windows.Forms.Button();
+            this.enviar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.NumeroCartasMano = new System.Windows.Forms.RadioButton();
+            this.puntuaciontotal = new System.Windows.Forms.RadioButton();
+            this.PuntuacionRonda = new System.Windows.Forms.RadioButton();
+            this.desconectar = new System.Windows.Forms.Button();
+            this.Registrarse = new System.Windows.Forms.RadioButton();
+            this.iniciarsesion = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(157, 49);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(164, 20);
+            this.nombre.TabIndex = 3;
+            // 
+            // Conectar
+            // 
+            this.Conectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Conectar.Location = new System.Drawing.Point(208, 447);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(149, 53);
+            this.Conectar.TabIndex = 4;
+            this.Conectar.Text = "conectar";
+            this.Conectar.UseVisualStyleBackColor = true;
+            // 
+            // enviar
+            // 
+            this.enviar.Location = new System.Drawing.Point(38, 447);
+            this.enviar.Name = "enviar";
+            this.enviar.Size = new System.Drawing.Size(124, 53);
+            this.enviar.TabIndex = 5;
+            this.enviar.Text = "Enviar";
+            this.enviar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Contraseña";
+            // 
+            // contraseña
+            // 
+            this.contraseña.Location = new System.Drawing.Point(157, 88);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(164, 20);
+            this.contraseña.TabIndex = 9;
+            // 
+            // NumeroCartasMano
+            // 
+            this.NumeroCartasMano.AutoSize = true;
+            this.NumeroCartasMano.Location = new System.Drawing.Point(424, 160);
+            this.NumeroCartasMano.Name = "NumeroCartasMano";
+            this.NumeroCartasMano.Size = new System.Drawing.Size(164, 17);
+            this.NumeroCartasMano.TabIndex = 7;
+            this.NumeroCartasMano.TabStop = true;
+            this.NumeroCartasMano.Text = "Número de cartas en la mano";
+            this.NumeroCartasMano.UseVisualStyleBackColor = true;
+            // 
+            // puntuaciontotal
+            // 
+            this.puntuaciontotal.AutoSize = true;
+            this.puntuaciontotal.Location = new System.Drawing.Point(424, 183);
+            this.puntuaciontotal.Name = "puntuaciontotal";
+            this.puntuaciontotal.Size = new System.Drawing.Size(196, 17);
+            this.puntuaciontotal.TabIndex = 7;
+            this.puntuaciontotal.TabStop = true;
+            this.puntuaciontotal.Text = "Puntuación total al final de la partida";
+            this.puntuaciontotal.UseVisualStyleBackColor = true;
+            // 
+            // PuntuacionRonda
+            // 
+            this.PuntuacionRonda.AutoSize = true;
+            this.PuntuacionRonda.Location = new System.Drawing.Point(424, 137);
+            this.PuntuacionRonda.Name = "PuntuacionRonda";
+            this.PuntuacionRonda.Size = new System.Drawing.Size(130, 17);
+            this.PuntuacionRonda.TabIndex = 8;
+            this.PuntuacionRonda.TabStop = true;
+            this.PuntuacionRonda.Text = "Puntos en cada ronda";
+            this.PuntuacionRonda.UseVisualStyleBackColor = true;
+            // 
+            // desconectar
+            // 
+            this.desconectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desconectar.Location = new System.Drawing.Point(394, 447);
+            this.desconectar.Name = "desconectar";
+            this.desconectar.Size = new System.Drawing.Size(147, 53);
+            this.desconectar.TabIndex = 10;
+            this.desconectar.Text = "desconectar";
+            this.desconectar.UseVisualStyleBackColor = true;
+            // 
+            // Registrarse
+            // 
+            this.Registrarse.AutoSize = true;
+            this.Registrarse.Location = new System.Drawing.Point(157, 148);
+            this.Registrarse.Name = "Registrarse";
+            this.Registrarse.Size = new System.Drawing.Size(153, 17);
+            this.Registrarse.TabIndex = 11;
+            this.Registrarse.TabStop = true;
+            this.Registrarse.Text = "Selecciona para registrarse";
+            this.Registrarse.UseVisualStyleBackColor = true;
+            // 
+            // iniciarsesion
+            // 
+            this.iniciarsesion.AutoSize = true;
+            this.iniciarsesion.Location = new System.Drawing.Point(157, 183);
+            this.iniciarsesion.Name = "iniciarsesion";
+            this.iniciarsesion.Size = new System.Drawing.Size(168, 17);
+            this.iniciarsesion.TabIndex = 12;
+            this.iniciarsesion.TabStop = true;
+            this.iniciarsesion.Text = "Selecciona para Iniciar Sesión";
+            this.iniciarsesion.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 563);
+            this.Controls.Add(this.iniciarsesion);
+            this.Controls.Add(this.Registrarse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.puntuaciontotal);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.NumeroCartasMano);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.desconectar);
+            this.Controls.Add(this.Conectar);
+            this.Controls.Add(this.enviar);
+            this.Controls.Add(this.PuntuacionRonda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.Button enviar;
+        private System.Windows.Forms.RadioButton NumeroCartasMano;
+        private System.Windows.Forms.RadioButton PuntuacionRonda;
+        private System.Windows.Forms.RadioButton puntuaciontotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.Button desconectar;
+        private System.Windows.Forms.RadioButton Registrarse;
+        private System.Windows.Forms.RadioButton iniciarsesion;
+    }
+}
+
---
+++ server.c
@@ -0,0 +1,373 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <mysql.h>
+
+int main(int argc, char *argv[])
+{
+	int sock_conn, sock_listen, ret;
+	struct sockaddr_in serv_adr;
+	char peticion[512];
+	char respuesta[512];
+	int error;
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	// Fem el bind al port
+	
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	
+	// asocia el socket a cualquiera de las IP de la m?quina. 
+	//htonl formatea el numero que recibe al formato necesario
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// escucharemos en el port 9050
+	serv_adr.sin_port = htons(9050);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	//La cola de peticiones pendientes no podr? ser superior a 4
+	if (listen(sock_listen, 2) < 0)
+		printf("Error en el Listen");
+	
+	int i;
+	// Atenderemos solo 7 peticione
+	for(;;){
+		printf ("Escuchando\n");
+		
+		sock_conn = accept(sock_listen, NULL, NULL);
+		printf ("He recibido conexi?n\n");
+		//sock_conn es el socket que usaremos para este cliente
+		
+		// Ahora recibimos su nombre, que dejamos en buff
+		ret=read(sock_conn,peticion, sizeof(peticion));
+		printf ("Recibido\n");
+		
+		// Tenemos que a?adirle la marca de fin de string 
+		// para que no escriba lo que hay despues en el buffer
+		peticion[ret]='\0';
+		
+		//Escribimos el nombre en la consola
+		
+		printf ("Se ha conectado: %s\n",peticion);
+		
+		
+		char *p = strtok( peticion, "/");
+		int codigo =  atoi (p);
+		p = strtok( NULL, "/");
+		char nombre[20];
+		strcpy (nombre, p);
+		printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+		
+		switch (codigo)
+		{
+
+		case 1:
+                //Registrar un usuario
+			
+			error = Registrarse(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			close(sock_conn); 
+				if (error != 0)
+					printf ("Ha ocurrido un error en el caso 1");
+		
+			break;
+			
+		case 2:
+			error = LogIn(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			close(sock_conn); 
+				if (error != 0)
+					printf ("Ha ocurrido un error en el caso 2");
+
+			break;
+		case 3:
+			error = PuntuacionRonda(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			close(sock_conn); 
+				if (error != 0)
+					printf ("Ha ocurrido un error en el caso 3");
+
+			break;
+		
+		case 4:
+			error = NumeroCartasMano(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			close(sock_conn); 
+			if (error != 0)
+				printf ("Ha ocurrido un error en el caso 4");
+
+			break;
+		case 5:
+			error = PuntuacionTotal(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			close(sock_conn); 
+			if (error != 0)
+				printf ("Ha ocurrido un error en el caso 5");
+			break;
+
+			
+		default:
+			// statements executed if expression does not equal
+			// any case constant_expression
+			break;
+		}
+	}
+	
+}
+
+int PuntuacionRonda (char nombre[20], char resultado[80])
+{
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado_c;
+	MYSQL_ROW row;
+	char consulta [80];
+
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexion
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "sushigo",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	sprintf (consulta, "SELECT ROUND_SCORE.SCORE FROM (ROUND_SCORE,PLAYER) WHERE PLAYER.NAME = '%s' AND ROUND_SCORE.ID_P = PLAYER.ID;",nombre);
+	
+	err=mysql_query (conn, consulta); 
+	if (err!=0) {
+		sprintf (resultado,"Error al consultar datos de la base %u %s",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//recogemos el resultado de la consulta 
+	resultado_c = mysql_store_result (conn); 
+	row = mysql_fetch_row (resultado_c);
+	
+	if (row == NULL) {
+		sprintf (resultado,"No se han obtenido datos en la consulta\n");
+	// Y lo enviamos 
+	}
+	else
+		strcpy(resultado,row[0]);
+
+		return 0;
+	
+	
+}
+
+int NumeroCartasMano(char nombre[20], char resultado[80])
+{
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado_c;
+	MYSQL_ROW row;
+	char Nombre_Jugador[20];
+	char consulta [80];
+
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexi\uffc3\uffb3n: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexion
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "sushigo",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexi\uffc3\uffb3n: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	printf ("\n Dame el nombre de la persona que quieres buscar\n"); 
+	scanf ("%s", Nombre_Jugador);
+	// construimos la consulta SQL
+	sprintf (consulta, "SELECT HAND.NUMBER_OF_CARDS FROM (HAND,PLAYER) WHERE PLAYER.NAME = '%s' AND HAND.ID_P = PLAYER.ID;",Nombre_Jugador);
+	// hacemos la consulta 
+	err=mysql_query (conn, consulta); 
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//recogemos el resultado de la consulta 
+	resultado_c = mysql_store_result (conn); 
+	row = mysql_fetch_row (resultado_c);
+	if (row == NULL){
+		sprintf (resultado,"No se han obtenido datos en la consulta\n");
+		// Y lo enviamos
+	}
+	else
+		sprintf(resultado,row[0]); 
+		return 0;
+}
+
+int PuntuacionTotal (char nombre[20], char resultado[80])
+{
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado_c;
+	MYSQL_ROW row;
+	char consulta [80];
+	
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexion
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "sushigo",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	sprintf (consulta, "SELECT GAME.TOTAL_SCORE FROM (GAME,PLAYER) WHERE PLAYER.NAME = '%s' AND GAME.ID_P = PLAYER.ID;",nombre);
+	
+	err=mysql_query (conn, consulta); 
+	if (err!=0) {
+		sprintf (resultado,"Error al consultar datos de la base %u %s",
+				 mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//recogemos el resultado de la consulta 
+	resultado_c = mysql_store_result (conn); 
+	row = mysql_fetch_row (resultado_c);
+	
+	if (row == NULL){
+		sprintf (resultado,"No se han obtenido datos en la consulta\n");
+	}
+	else
+		strcpy(resultado,row[0]);
+	return 0;
+	
+}
+	int Registrarse (char usuario[20], char contrasena[20]) {	
+	MYSQL *conn;
+	MYSQL_RES *resultado_c;
+	MYSQL_ROW row;
+	int err;
+	char consulta [80];
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexion: %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexion
+
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "sushigo",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	err=mysql_query (conn, "SELECT * FROM PLAYER");
+	if (err!=0) {
+		printf ("Error al consultar la base de datos %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+
+	resultado_c = mysql_store_result (conn);
+	int i = 0;
+	row = mysql_fetch_row (resultado_c);
+	while(row != NULL) {
+		i++;
+		row = mysql_fetch_row (resultado_c);
+	}
+	strcpy(consulta, "SELECT * FROM PLAYER WHERE PLAYER.NAME ='");
+	strcat(consulta, usuario);
+	strcat(consulta, "'");
+	err=mysql_query (conn, consulta);
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	resultado_c = mysql_store_result (conn);
+	row = mysql_fetch_row (resultado_c);
+	if(row != NULL) {
+		printf("Error. Ya hay alguien con ese nombre.");
+		exit (1);
+	}
+	sprintf(consulta, "INSERT INTO PLAYER VALUES (%d,'%s','%s',0);", i + 1, usuario, contrasena);
+	
+	printf("consulta = %s\n", consulta);
+	// Ahora ya podemos realizar la insercion 
+	err = mysql_query(conn, consulta);
+	if (err!=0) {
+		printf ("Error al introducir datos la base %u %s\n", 
+		mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+
+	// cerrar la conexion con el servidor MYSQL 
+	mysql_close (conn);
+	return 0;
+	
+}
+
+int LogIn(char usuario[20], char contrasena[20]) {
+	MYSQL *conn;
+	MYSQL_RES *resultado_c;
+	MYSQL_ROW row;
+	int err;
+	char consulta [80];
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexion: %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexiￃﾳn, entrando nuestras claves de acceso y
+	//el nombre de la base de datos a la que queremos acceder 
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "sushigo",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	strcpy(consulta, "SELECT * FROM PLAYER WHERE PLAYER.NAME='");
+	strcat(consulta, usuario);
+	strcat(consulta, "' AND PLAYER.PASSWORD='");
+	strcat(consulta, contrasena);
+	strcat(consulta, "'");
+	err=mysql_query (conn, consulta);
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	resultado_c = mysql_store_result (conn);
+	row = mysql_fetch_row (resultado_c);
+	if(row == NULL) {
+		printf("Error. Los datos no coinciden.");
+		exit (1);
+	}
+	// cerrar la conexion con el servidor MYSQL 
+	mysql_close (conn);
+	return 0;
+}
