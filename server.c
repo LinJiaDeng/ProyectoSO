605f7a0e4bf746c2323cd01b77b6493fa4c8ce7a
guille pacheco <byguille6@gmail.com>
2021-04-22 13:38:54 +0200

    v2

--- server.c
+++ server.c
@@ -6,6 +6,214 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <mysql.h>
+#include <pthread.h>
+
+typedef struct {
+	char nombre[20];
+	int socket;
+}Conectado;
+
+typedef struct {
+	Conectado conectados [100];
+	int num;
+}ListaConectados;
+
+ListaConectados lista;
+
+int Conectarse (ListaConectados *lista, char nombre[20], int socket){
+	if (lista->num == 100)
+		return -1;
+	else {
+		strcpy (lista->conectados[lista->num].nombre, nombre);
+		lista->conectados[lista->num].socket = socket;
+		lista->num++;
+		return 0;
+	}
+}
+
+int DameSocket (ListaConectados *lista, char nombre[20]){
+	//Devueleve el socket
+	int i=0;
+	int encontrado = 0;
+	while ((i< lista->num) && !encontrado)
+	{
+		if (strcmp(lista->conectados[i].nombre, nombre) == 0)
+			encontrado =1;
+		if (!encontrado)
+			i=i+1;
+	}
+	if (encontrado)
+		return lista->conectados[i].socket;
+	else
+		return -1;
+}
+
+
+int DamePosicion (ListaConectados *lista, char nombre[20]){
+	//Devueleve la posicion
+	int i=0;
+	int encontrado = 0;
+	while ((i< lista->num) && !encontrado)
+	{
+		if (strcmp(lista->conectados[i].nombre, nombre) == 0)
+			encontrado =1;
+		if (!encontrado)
+			i=i+1;
+	}
+	if (encontrado)
+		return i;
+	else
+		return -1;
+}
+
+
+int Eliminar (ListaConectados *lista, char nombre[20]){
+	//Retorna 0 si elimina y -1 si el usuario no estï¿¡ en la lista
+	int pos = DamePosicion (lista, nombre);
+	if (pos == -1)
+		return -1;
+	else
+	{
+		int i;
+		for (i=pos; i< lista->num-1; i++)
+		{
+			strcpy (lista->conectados[i].nombre, lista->conectados[i+1].nombre);
+			lista->conectados[i].socket = lista->conectados[i+1].socket;          
+		}
+		lista->num--;
+		return 0;
+	}
+}
+
+
+void DameConectados (ListaConectados *lista, char conectados[500]){
+	//Devuelve los nombres de los conectados separados por /.
+	sprintf (conectados, "Numero de personas conectadas: %d \n", lista->num);
+	int i;
+	for (i=0; i< lista->num; i++)
+	{
+		sprintf (conectados, "%s \n %s", conectados, lista->conectados[i].nombre);
+	}
+}
+
+void *AtenderCliente (void *socket)
+{
+
+	int sock_conn;
+	int *s;
+	s= (int *) socket;
+	sock_conn= *s;
+	int error;
+	
+	//int socket_conn = * (int *) socket;
+	
+	char peticion[512];
+	char respuesta[512];
+	int ret;
+	
+	int terminar =0;
+	// Entramos en un bucle para atender todas las peticiones de este cliente
+	//hasta que se desconecte
+	while (terminar ==0)
+	{
+		// Ahora recibimos su nombre, que dejamos en buff
+		ret=read(sock_conn,peticion, sizeof(peticion));
+		printf ("Recibido\n");
+		
+		
+		// Tenemos que a?adirle la marca de fin de string 
+		// para que no escriba lo que hay despues en el buffer
+		peticion[ret]='\0';
+		
+		//Escribimos el nombre en la consola
+		printf ("Se ha conectado: %s\n",peticion);
+		
+		char *p = strtok( peticion, "/");
+		int codigo =  atoi (p);
+		char nombre[20];
+		p = strtok( NULL, "/");
+			
+		strcpy (nombre, p);
+		printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+				
+		switch (codigo)
+		{
+		case 0:
+			terminar = 1;
+			Eliminar(&lista,nombre);
+			close(sock_conn);
+			break;
+		case 1:
+			//Registrar un usuario
+			p = strtok( NULL, "/");
+			char contrasena[20];
+			strcpy (contrasena, p);
+			error = Registrarse(nombre,contrasena);
+			write (sock_conn,nombre, strlen(nombre));
+			
+			if (error != 0)
+				printf ("Ha ocurrido un error en el caso 1");
+			
+			break;
+			
+		case 2:
+			//Iniciar Sesion
+			p = strtok( NULL, "/");
+			
+			strcpy (contrasena, p);
+			error = LogIn(nombre,contrasena);
+			write (sock_conn,nombre,strlen(nombre));
+			int numsocket = DameSocket(&lista,nombre);
+			int errorCON = Conectarse (&lista,nombre,numsocket);
+			if (error != 0)
+				printf ("Ha ocurrido un error en el caso 2");
+			if (errorCON != 0)
+				printf ("Ha ocurrido un error en el caso 1, no se ha podido añadir a la lista de conectados");
+			
+			break;
+		case 3:
+			error = PuntuacionRonda(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			
+			if (error != 0)
+				printf ("Ha ocurrido un error en el caso 3");
+			
+			break;
+			
+		case 4:
+			error = NumeroCartasMano(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			
+			if (error != 0)
+				printf ("Ha ocurrido un error en el caso 4");
+			
+			break;
+		case 5:
+			error = PuntuacionTotal(nombre,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			
+			if (error != 0)
+				printf ("Ha ocurrido un error en el caso 5");
+			break;
+		case 6:
+			DameConectados(&lista,respuesta);
+			write (sock_conn,respuesta, strlen(respuesta));
+			break;
+			
+			
+		default:
+			
+			// statements executed if expression does not equal
+			// any case constant_expression
+			break;
+		}
+		
+	}
+	// Se acabo el servicio para este cliente
+	close(sock_conn); 
+	
+}
+
 
 int PuntuacionRonda (char nombre[20], char resultado[150])
 {
@@ -250,11 +458,11 @@ int LogIn(char usuario[20], char contrasena[20]) {
 
 int main(int argc, char *argv[])
 {
+	lista.num=0;
 	int sock_conn, sock_listen, ret;
 	struct sockaddr_in serv_adr;
 	char peticion[512];
 	char respuesta[512];
-	int error;
 	// INICIALITZACIONS
 	// Obrim el socket
 	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -276,101 +484,22 @@ int main(int argc, char *argv[])
 	if (listen(sock_listen, 2) < 0)
 		printf("Error en el Listen");
 	
-	
-	printf ("Escuchando\n");
-	
-	sock_conn = accept(sock_listen, NULL, NULL);
-	printf ("He recibido conexion\n");
-	//sock_conn es el socket que usaremos para este cliente
+	int i;
+	int sockets[100];
+	pthread_t thread;
+	i=0;
 	
 	for(;;){
+		printf ("Escuchando\n");
 		
-		// Ahora recibimos su nombre, que dejamos en buff
-		ret=read(sock_conn,peticion, sizeof(peticion));
-		printf ("Recibido\n");
+		sock_conn = accept(sock_listen, NULL, NULL);
+		printf ("He recibido conexion\n");
 		
-		// Tenemos que a?adirle la marca de fin de string 
-		// para que no escriba lo que hay despues en el buffer
-		peticion[ret]='\0';
+		sockets[i] =sock_conn;
 		
-		//Escribimos el nombre en la consola
-		printf ("Se ha conectado: %s\n",peticion);
+		pthread_create (&thread, NULL, AtenderCliente,&sockets[i]);
+		i=i+1;
 		
-		char *p = strtok( peticion, "/");
-		int codigo =  atoi (p);
-		char nombre[20];
-		if(codigo!=0){
-			p = strtok( NULL, "/");
-			
-			strcpy (nombre, p);
-			printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
-		}
-		else
-		   codigo=0;
-		
-		switch (codigo)
-		{
-		case 0:
-			close(sock_conn);
-			break;
-		case 1:
-                //Registrar un usuario
-			p = strtok( NULL, "/");
-			char contrasena[20];
-			strcpy (contrasena, p);
-			error = Registrarse(nombre,contrasena);
-			write (sock_conn,nombre, strlen(nombre));
-			 
-				if (error != 0)
-					printf ("Ha ocurrido un error en el caso 1");
-		
-			break;
-			
-		case 2:
-			p = strtok( NULL, "/");
-			
-			strcpy (contrasena, p);
-			error = LogIn(nombre,contrasena);
-			write (sock_conn,nombre,strlen(nombre));
-			 
-				if (error != 0)
-					printf ("Ha ocurrido un error en el caso 2");
-
-			break;
-		case 3:
-		    printf("hola");
-			error = PuntuacionRonda(nombre,respuesta);
-			printf("adios");
-			write (sock_conn,respuesta, strlen(respuesta));
-			 
-				if (error != 0)
-					printf ("Ha ocurrido un error en el caso 3");
-
-			break;
-		
-		case 4:
-			error = NumeroCartasMano(nombre,respuesta);
-			write (sock_conn,respuesta, strlen(respuesta));
-			
-			if (error != 0)
-				printf ("Ha ocurrido un error en el caso 4");
-
-			break;
-		case 5:
-			error = PuntuacionTotal(nombre,respuesta);
-			write (sock_conn,respuesta, strlen(respuesta));
-			
-			if (error != 0)
-				printf ("Ha ocurrido un error en el caso 5");
-			break;
-
-			
-		default:
-			
-				// statements executed if expression does not equal
-			// any case constant_expression
-			break;
-		}
 	}
 	
 }
